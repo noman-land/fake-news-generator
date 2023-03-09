26c48737703e5c406c44c1fb4b300ba3adab26bd
Noman <noman@noman.land>
2023-03-09 14:19:10 -0500

    Make better

--- README.md
+++ README.md
@@ -3,5 +3,12 @@
 URL format:
 
 ```
-https://domain.com/{year}/{month}/{day}/{siteName}/{headline}/{description}/{base64ImageUrl}
+https://domain.com/{topic}/{year}/{month}/{day}/{headline}/{base64Description}/{base64ImageUrl}?fbshid={base64Props}
+```
+
+For local dev you must have `.dev.vars` file with the following items:
+
+```
+SITE_NAME=Example Site
+ICON_URL=https://images.example.com/favicon.ico
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,98 @@
+interface Env {
+  SITE_NAME: string;
+  ICON_URL: string;
+}
+
+const pad = (num: number) => num.toString().padStart(2, '0');
+
+const wait = async (ms: number) =>
+  new Promise(resolve => {
+    setTimeout(async () => resolve(true), ms);
+  });
+
 const html = ({
   headline,
   description,
   imageUrl,
   iconUrl,
   siteName,
-  date,
+  year,
+  month,
+  day,
+  props,
 }: {
   headline: string;
   description: string;
   imageUrl: string;
   iconUrl: string;
   siteName: string;
-  date: string;
-}) => `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>${headline}</title>
-      <meta charset="utf-8">
-      <meta property="og:title" content="${headline}" />
-      <meta property="og:description" content="${description}" />
+  year: number;
+  month: number;
+  day: number;
+  props: Object;
+}) => {
+  const y = pad(year);
+  const m = pad(month);
+  const d = pad(day);
+  const hh = pad((year * month * day) % 24);
+  const mm = pad((year * day) % 60);
+  const ss = pad((year * month * headline.length) % 60);
+  const timestamp = `${y}-${m}-${d}T${hh}:${mm}:${ss}Z`;
 
-      <meta property="twitter:label1" content="Published">
-      <meta property="twitter:data1" content="${date}">
-      <meta property="twitter:label2" content="Reading time">
-      <meta property="twitter:data2" content="6 minutes">
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>${headline}</title>
+        <meta charset="utf-8">
+        <meta property="og:title" content="${headline}" />
+        <meta property="og:description" content="${description}" />
+        <meta name="article:published_time" content="${timestamp}">
 
-      <meta property="og:image" content="${imageUrl}" />
-      <meta property="og:type" content="article">
-      <meta property="og:site_name" content="${siteName}">
-      <meta property="og:locale" content="en_US">
+        <meta property="og:image" content="${imageUrl}" />
+        <meta property="og:type" content="article">
+        <meta property="og:site_name" content="${siteName}">
+        <meta property="og:locale" content="en_US">
 
-      <link rel="shortcut icon" href="${iconUrl}">
-    </head>
-    <body>
-      <div>You must enable Javascript to view this page.</div>
-    </body>
-  </html>
-`;
+        ${Object.entries(props)
+          .map(
+            ([k, v], i) => `
+              <meta property="twitter:label${i + 1}" content="${k}">
+              <meta property="twitter:data${i + 1}" content="${v}">
+            `
+          )
+          .join('')}
+
+        <link rel="shortcut icon" href="${iconUrl}">
+      </head>
+      <body>
+        <div>You must enable Javascript to view this page.</div>
+      </body>
+    </html>
+  `;
+};
 
 export default {
-  async fetch(request: Request): Promise<Response> {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    // await wait(5000);
+
     try {
       const { pathname, searchParams } = new URL(request.url);
-      const { v: iconUrl } = Object.fromEntries(
-        new URLSearchParams(searchParams).entries()
-      );
-      const [, year, month, day, siteName, headline, description, imageUrl] =
-        pathname.split('/');
+      const [_topic, year, month, day, headline, description, imageUrl] =
+        pathname.slice(1).split('/');
+      const fbshid = searchParams.get('fbshid');
 
       return new Response(
         html({
           headline: decodeURIComponent(headline),
-          description: decodeURIComponent(description),
-          siteName: decodeURIComponent(siteName),
+          description: atob(description),
           imageUrl: atob(imageUrl),
-          iconUrl: atob(iconUrl),
-          date: new Date(`${year}-${month}-${day}`)
-            .toUTCString()
-            .replace('00:00:00 GMT', '8:42 AM'),
+          iconUrl: env.ICON_URL,
+          siteName: env.SITE_NAME,
+          year: parseInt(year, 10),
+          month: parseInt(month, 10),
+          day: parseInt(day, 10),
+          props: fbshid ? JSON.parse(atob(fbshid)) : {},
         }),
         {
           headers: {
@@ -67,6 +101,7 @@ export default {
         }
       );
     } catch (e) {
+      console.log('\n\n', e, '\n\n');
       return new Response('Not found', { status: 404 });
     }
   },
